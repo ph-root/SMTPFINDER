32af7aa233e9d57bfaaefcf03a1443a331689c89
beneameenth <43159457+beneameenth@users.noreply.github.com>
2018-09-11 15:33:30 +0200

    Add files via upload

--- smtpfinder.py
+++ smtpfinder.py
@@ -1,3 +1,4 @@
+#! python3
 import requests , os , bs4 , re , random , shelve , getpass , webbrowser , sys
 
 if sys.platform in ["linux","linux2"]:
@@ -9,10 +10,21 @@ else:
 	G = ''
 	R = ''
 
+
+retries = shelve.open('logging')
+if  retries['max_num'] - retries['num'] == 4:
+	try:
+		import update
+	except:
+		print(G + '[*] please check your connection to the internet')
+		print('[*] GoodBye ... ')
+		exit()
+retries.close()
+
 print(R + '''+++++++++++++++++++++++++++++++++++++++++++++++++
-+                 SMTP FINDER                   +
-+                CODER : BEN_TH                 +
-+           WHATSAPP : +201006698345            +
++                SMTP FINDER                    +
++              CODER : BEN_TH                   +
++          WHATSAPP : +201006698345             +
 + please Dont use this script in spam (illegal) +
 +    FB : www.facebook.com/bassem.beso.18659    +
 +++++++++++++++++++++++++++++++++++++++++++++++++''')
@@ -35,7 +47,8 @@ def chk():
 		# print('pass : ' , end = '')
 		pass1 = getpass.getpass('Password:')
 		if pass1 == log['users'][user1]:
-			print(G + '[*] You are logged in as %s'% (user1))
+			print(G + '[*] You are logged in as %s and you  have %s retries'% (user1,log['max_num'] - log['num']))
+
 		elif pass1 != log['users'][user1]:
 			print('the password is wrong !')
 			exit()
